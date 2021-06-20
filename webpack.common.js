265f53d68be1c8bb2f4cf1bd4889e1ece1b245c7
cbowen <cbowen@msu.edu>
2021-06-19 22:03:31 -0400

    Updates for webpack 5 and yarn 2

--- webpack.common.js
+++ webpack.common.js
@@ -49,7 +49,12 @@ module.exports = {
                     'style-loader',
                     'css-loader',
                     'resolve-url-loader',
-                    'sass-loader?sourceMap'
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    }
                 ]
             },
             {
